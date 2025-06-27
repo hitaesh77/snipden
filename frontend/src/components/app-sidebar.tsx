d08ccb19f141803fc2b1c1cbc9edea0c8bdaebd0
hitaesh77 <hitaesh777@gmail.com>
2025-06-27 14:54:05 -0400

    started sidebar and add snippet page

---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import * as React from "react"
+import {
+  Home,
+  FileCode,
+  Plus,
+  Search,
+  Settings
+} from "lucide-react"
+import Link from 'next/link'
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "My Snippets",
+    url: "#",
+    icon: FileCode,
+  },
+  {
+    title: "Add Snippet",
+    url: "/snippets/add",
+    icon: Plus,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+// This is sample data for user
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  }
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarGroupLabel className="text-black font-mono font-bold text-lg">SnipDen</SidebarGroupLabel>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url} className="w-full">
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
