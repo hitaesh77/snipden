9ed5d6afc12a9aebab7751bd1ca1580112160f13
hitaesh77 <hitaesh777@gmail.com>
2025-06-27 23:44:32 -0400

    added get snippets endpoint

--- backend/app/routes/snippets.py
+++ backend/app/routes/snippets.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from uuid import UUID
-from app.schemas.schemas import SnippetCreate, Snippet
+from app.schemas.schemas import SnippetCreate, SnippetBase, Snippet
 from app.services import crud
-from app.db import db
+from app.db import db, models
 
 router = APIRouter()
 
-@router.post("/snippets/", response_model=Snippet)
+@router.post("/snippets/add", response_model=Snippet)
 def create_snippet(snippet: SnippetCreate, db: Session = Depends(db.get_db)):
     return crud.create_snippet(db=db, snippet=snippet)
+
+@router.get("/snippets", response_model=list[Snippet])
+def get_snippets(db: Session = Depends(db.get_db)):
+    return db.query(models.Snippet).all()
 
 '''
 @router.post("/snippets/", response_model=Snippet)
